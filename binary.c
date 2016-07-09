e901b044b8fd7a67525290802502c4fd5505268a
gomathikittusamy <gomsanu4@gmail.com>
2016-07-09 09:03:42 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i=1,n,b=0;
+clrscr();
+scanf("%d",&n);
+while(n!=0)
+{
+a=n%2;
+n=n/2;
+bin=bin+r*i;
+a*=10;
+}
+printf("%d ",bin);
+
+getch();
+}
